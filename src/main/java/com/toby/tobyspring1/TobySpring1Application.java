bf71e236b584bbb669fcb9296da978ff8240f875
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-11-10 17:00:18 +0900

    add spring

---
+++ src/main/java/com/toby/tobyspring1/TobySpring1Application.java
@@ -0,0 +1,13 @@
+package com.toby.tobyspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/toby/tobyspring1/TobySpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.tobyspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
