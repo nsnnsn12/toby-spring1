13c2215a739d4e0be25c9abf4a64ee6013504a4a
nsnnsn12 <gkdlshtjdrb@naver.com>
2022-11-10 11:22:57 +0900

    init toby-spring

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -2,20 +2,34 @@ package user.dao;
 
 import user.domain.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
-    public UserDao(){
-        connectionMaker = new DConnectionMaker();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("sunggyu");
+        user.setName("노성규");
+        user.setPassword("shtjdrb");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
     }
     public void add(User user) throws ClassNotFoundException, SQLException{
-        Connection connection = connectionMaker.makeConnection();
+        Class.forName ("org.h2.Driver");
+        Connection c = DriverManager.getConnection("jdbc:h2:~/toby", "sa","");
 
-        PreparedStatement ps = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
@@ -23,13 +37,14 @@ public class UserDao {
         ps.executeUpdate();
 
         ps.close();
-        connection.close();
+        c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Connection connection = connectionMaker.makeConnection();
+        Class.forName ("org.h2.Driver");
+        Connection c = DriverManager.getConnection("jdbc:h2:~/toby", "sa","");
 
-        PreparedStatement ps = connection.prepareStatement("select * from users where id = ?");
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
@@ -41,7 +56,7 @@ public class UserDao {
 
         rs.close();
         ps.close();
-        connection.close();
+        c.close();
 
         return user;
     }
